9532766f81129fd507b16ac1cca187890b6a3aeb
hurlx <n247684@gmail.com>
2025-07-21 04:52:55 +0300

    Update data.jsx

--- app/Lib/data.jsx
+++ app/Lib/data.jsx
@@ -1,12 +1,12 @@
   export const products = [
     {
-      name: "حقيبة خاصة ب Rize",
+      name: "Rize Bag",
       shortDescription: "حقيبة يد أنيقة بنقشة الفهد ولمسات جلدية بنية، مزودة بسلسلة ذهبية تضيف لمسة فاخرة.",
       image: "/images/1/Kaki.jpg",
       hoverImage: "/images/1/Coffee.jpg",
       price: 20,
       id: "1",
-      slug: "gg",
+      slug: "Rize",
       images: [
         "/images/1/Coffee.jpg",
         "/images/1/Brown.jpg",
@@ -19,13 +19,13 @@
       ],
     },
     {
-      name: "Wavea",
+      name: "Wavea Bag",
       shortDescription: "حقيبة يد جلدية أنيقة ، بتصميم مموّج يدوي ومقبض علوي مجعد، مزودة بسلسلة ذهبية ووسم فاخر.",
       image: "/images/2/Kaki.jpg",
       hoverImage: "/images/2/White.jpg",
       price: 17,
       id: "2",
-      slug: "aa",
+      slug: "Wavea",
       images: [
         "/images/2/Black.jpg",
         "/images/2/Kaki.jpg",
@@ -40,13 +40,13 @@
       ],
     },
     {
-      name: "حقيبة القش",
+      name: "straw Bag",
       shortDescription: "حقيبة كروس أنيقة مصنوعة من القش الطبيعي مع غطاء جلدي ، بتفاصيل مضفّرة ولمسة عصرية من الشرّابات والحبل القطني.",
       image: "/images/4/Black.jpg",
       hoverImage: "/images/4/White.jpg",
       price: 22,
       id: "3",
-      slug: "ab",
+      slug: "straw",
       images: [
         "/images/4/Black.jpg",
         "/images/4/Brown.jpg",
@@ -58,6 +58,95 @@
         { name: "Black", hex: "#000000" },
         { name: "Brown", hex: "#964B00" },
         { name: "Yellow", hex: "#FFFF00" }
+      ],
+    },
+    {
+      name: "Shell Bag",
+      shortDescription: "حقيبة جلدية عريقة , مصنوعة بالكامل من الجلد مع بعض اللمسات الذهبية التي تعطي طابع فخم",
+      image: "/images/5/Black.jpg",
+      hoverImage: "/images/5/Brawn.jpg",
+      price: 23,
+      id: "4",
+      slug: "Shell",
+      images: [
+        "/images/5/Black.jpg",
+        "/images/5/Brawn.jpg",
+        "/images/5/1.jpg",
+        "/images/5/2.jpg"
+      ],
+      colors: [
+        { name: "White", hex: "#ffffff" },
+        { name: "Black", hex: "#000000" },
+      ],
+    },
+    {
+      name: "Diamond Bag",
+      shortDescription: "حقيبة الألماس المميزة , ذات طابع الماسي يحتوي الوان الطيف السبع , وتحتوي على سلسلة تعطي طابع كلاسيكي",
+      image: "/images/6/Rainbow.jpg",
+      hoverImage: "/images/6/White.jpg",
+      price: 20,
+      id: "5",
+      slug: "Diamond",
+      images: [
+        "/images/6/Rainbow.jpg",
+        "/images/6/White.jpg",
+        "/images/6/1.jpg",
+        "/images/6/2.jpg",
+        "/images/6/3.jpg"
+      ],
+      colors: [
+        { name: "White", hex: "#ffffff" },
+        { name: "Rainbow", hex: "#000000" },
+      ],
+    },
+    {
+      name: "Gold Cat Bag",
+      shortDescription: "حقيبة القطة الذهبية بتصميم عصري بسيط , تحتوي على حزام مزخرف , وتفاصيل ذهبية تعطي طابع فخم",
+      image: "/images/7/Black.jpg",
+      hoverImage: "/images/7/Pink.jpg",
+      price: 18,
+      id: "6",
+      slug: "Cat",
+      images: [
+        "/images/7/Black.jpg",
+        "/images/7/Pink.jpg",
+        "/images/7/1.jpg",
+        "/images/7/2.jpg",
+        "/images/7/3.jpg",
+        "/images/7/4.jpg",
+        "/images/7/Blue.jpg",
+        "/images/7/Kaki&Pink.jpg",
+        "/images/7/Green.jpg",
+        "/images/7/Yellow.jpg",
+      ],
+      colors: [
+        { name: "Kaki", hex: "#F0E68C" },
+        { name: "Black", hex: "#000000" },
+        { name: "Blue", hex: "#0000FF" },
+        { name: "Green", hex: "#008000" },
+        { name: "Pink", hex: "#FFC0CB" },
+        { name: "Yellow", hex: "#FFFF00" },
+      ],
+    },
+    {
+      name: "Canvas Bucket Bag",
+      shortDescription: "حقيبة القماش المقلم , قماش مخطط ذات ملمس خشن , بتصميم راقي , مع لمسات ذهبية تعطي طابع الفخامة",
+      image: "/images/5/Black.jpg",
+      hoverImage: "/images/5/Brawn.jpg",
+      price: 18,
+      id: "7",
+      slug: "Canvas",
+      images: [
+        "/images/9/Black.jpg",
+        "/images/9/Braown.jpg",
+        "/images/9/Blue.jpg",
+        "/images/9/Red.jpg"
+      ],
+      colors: [
+        { name: "Brown", hex: "#964B00" },
+        { name: "Black", hex: "#000000" },
+        { name: "Blue", hex: "#0000FF" },
+        { name: "Red", hex: "#FF0000" },
       ],
     },
   ];
