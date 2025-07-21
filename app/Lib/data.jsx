95a180230572ce09db3a026ddb0229795c3f94b3
hurlx <n247684@gmail.com>
2025-07-21 14:23:53 +0300

    Update data.jsx

--- app/Lib/data.jsx
+++ app/Lib/data.jsx
@@ -38,7 +38,7 @@
         "/images/2/Phosphorescent.jpg",
         "/images/2/(1).jpg",
         "/images/2/(2).jpg",
-        "/images/3/(3).jpg",
+        "/images/2/(3).jpg",
       ],
       colors: [
         { name: "White", hex: "#ffffff" },
