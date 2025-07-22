0fd323ffdd8a35095852567cd965ab7e2ca0f16e
hurlx <n247684@gmail.com>
2025-07-22 21:10:47 +0300

    Update Top.jsx

--- components/Top.jsx
+++ components/Top.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div className='flex items-center text-2xl flex-col justify-center pt-3 text-white'>
+    <div className='flex items-center text-xl flex-col justify-center pt-3 text-gray-500'>
         <h3>شحن مجاني الى جميع المحافظات</h3>
         <h3>توصيل مجاني الى باب المنزل في حمص</h3>
     </div>
