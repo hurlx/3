23aecebc0284ad0f92b2c47df5dc63b60eaeccf1
hurlx <n247684@gmail.com>
2025-07-22 20:58:00 +0300

    Create Top.jsx

---
+++ components/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className='flex items-center text-2xl flex-col justify-center pt-3 text-white'>
+        <h3>شحن مجاني الى جميع المحافظات</h3>
+        <h3>توصيل مجاني الى باب المنزل في حمص</h3>
+    </div>
+  )
+}
+
+export default Top
