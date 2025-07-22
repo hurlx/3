325513601ee29739a8d391c8278e06e5d5fa106f
hurlx <n247684@gmail.com>
2025-07-22 21:01:18 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased select-none`}
       >
-        <div className="absolute top-0 left-0 z-[9999] pointer-events-auto w-fit mx-auto right-0">
+        <div className="relative top-0 left-0 z-[9999] pointer-events-auto w-fit mx-auto right-0">
   <Link href="/">
     <Image
       src="/images/Hight Qulity.png"
