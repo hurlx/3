3bed4b915d555e81264cb3a3e9879f8a9bf627cf
hurlx <n247684@gmail.com>
2025-07-24 17:41:01 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,22 +18,22 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "RiZe",
-  icons: {
-    icon: [
-      { rel: "icon", url: "/favicon.ico", type: "image/x-icon" },
-    ],
-  },
   description:
     "Discover stylish and high-quality bags crafted with care. Perfect for daily use, travel, or gifts — designed to suit every personality.",
-    
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+        sizes: "any",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased select-none`}
       >
