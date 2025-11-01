d77e8e11e5b47a7db63cb8f987fa0219d8c527ca
hurlx <n247684@gmail.com>
2025-11-01 18:49:48 +0300

    Update index.jsx

--- constants/index.jsx
+++ constants/index.jsx
@@ -1,53 +1,8 @@
 const navLinks = [
-  {
-    name: "رجال",
-    link: "#man",
-  },
-  {
-    name: "نساء",
-    link: "#woman",
-  },
-  {
-    name: "مدرسي",
-    link: "#schoole",
-  },
-  { 
-    name: "رياضي",
-    link: "#sport",
-  },
-];
-
-const abilities = [
-  {
-    title: "⭐⭐⭐⭐⭐",
-    desc: "الطلب وصلني بسرعة والتغليف كتير مرتب الشنتة طالعة أحلى من الصورة بكتير عنجد مبسوطة وناوية أطلب كمان",
-    id: "1",
-  },
-  {
-    title: "⭐⭐⭐⭐",
-    desc: "الشنتاية فخمة والسعر كويس تاخرت بالشحن شوي بس مو مشكلة المهم الجودة بتجنن وبنصح فيه",
-    id: "2",
-  },
-  {
-    title: "⭐⭐⭐⭐⭐",
-    desc: "طلبت حقيبة وطلعت بتجننن الخامات ممتازة واللون متل الصور تماما والتوصيل كتير سريع أكيد مو آخر مرة بطلب منكم",
-    id: "3",
-  },
-];
-
-const socialImgs = [
-  { imgPath: "/icons/house-blank.svg", name: "Home" },
-  { imgPath: "/icons/shopping-bag.svg", name: "Bag" },
-  { imgPath: "/icons/instagram.svg", url: "https://www.instagram.com/rizemybag/", name: "Instagram" },
-  { imgPath: "/icons/facebook.svg", url: "https://www.facebook.com/share/1DpFyALrGX/", name: "Facebook" },
-  { imgPath: "/icons/whatsapp.svg", name: "WhatsApp", url: "https://wa.me/963997206837", },
-  //{ imgPath: "/icons/square-info.svg", name: "Info" },
-
-  
+  { label: "الحقائب", href: "/Bag" },
+  { label: "الرئيسية", href: "/" },
 ];
 
 export {
   navLinks,
-  socialImgs,
-  abilities,
 };
