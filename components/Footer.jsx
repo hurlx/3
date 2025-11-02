24d24b998a56e9f3653c67f1958350801210f08d
hurlx <n247684@gmail.com>
2025-11-02 13:17:22 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,11 @@ export default function Footer() {
   return (
     <footer dir="rtl" className="bg-black text-white px-6 py-12 md:py-20">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row md:justify-between md:items-start gap-12 md:gap-24 text-center md:text-right">
-        
         {/* Brand Section */}
         <div className="flex-1 space-y-4">
-          <h2 className="text-4xl font-extrabold tracking-tight uppercase">Rize</h2>
+          <h2 className="text-4xl font-extrabold tracking-tight uppercase">
+            Rize
+          </h2>
           <p className="text-gray-400 text-sm leading-relaxed max-w-sm md:max-w-md mx-auto md:mx-0">
             حقيبتك ليست مجرد إكسسوار — إنها تعبير عن أسلوبك، تميزك، وثقتك.
           </p>
@@ -22,7 +23,7 @@ export default function Footer() {
             <a
               href="https://www.instagram.com/rizemybags?igsh=MW93ZmNpdXIybGlkbw=="
               target="_blank"
-              rel="noopener noreferrer"
+              rel="Instagram"
               aria-label="Instagram"
               className="text-gray-400 hover:text-white hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.6)] transition-all duration-300"
             >
@@ -31,19 +32,21 @@ export default function Footer() {
             <a
               href="https://wa.me/963997206837"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="WhatsApp"
               aria-label="WhatsApp"
               className="text-gray-400 hover:text-white hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.6)] transition-all duration-300"
             >
               <FaWhatsapp size={24} />
             </a>
-            <a 
-    href="https://www.facebook.com/share/14MTnhCys6H/" 
-    aria-label="Facebook" 
-    className="text-gray-400 hover:text-gray-200 transition-transform duration-300 hover:scale-110"
-  >
-    <FaFacebookF size={24} />
-  </a>
+            <a
+              href="https://www.facebook.com/share/14MTnhCys6H/"
+              target="_blank"
+              rel="Facebook"
+              aria-label="Facebook"
+              className="text-gray-400 hover:text-gray-200 transition-transform duration-300 hover:scale-110"
+            >
+              <FaFacebookF size={24} />
+            </a>
           </div>
         </div>
       </div>
